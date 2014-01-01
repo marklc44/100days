5cf29b057f918f4b85ad0d73731afbc87235d6f1
Mark Centoni <marklc44@gmail.com>
2013-12-31 17:30:53 -0800

    Added Simple Project Tracker v0.1

---
+++ project-tracker/js/main.js
@@ -0,0 +1,3 @@
+https://docs.google.com/spreadsheet/ccc?key=0AmwKaFcFlMxjdEMzR3FNUm4zRFNOZ3dyNUMxOHpOeHc&usp=sharing
+
+http://spreadsheets.google.com/feeds/feed/0AmwKaFcFlMxjdEMzR3FNUm4zRFNOZ3dyNUMxOHpOeHc/100days/public/basic?alt=json-in-script&callback=displayProjectCalendar
